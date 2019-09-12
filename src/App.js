7a00dd3160577d855ed8dd9a15e5449a4ca42a0a
brandiw <brandimichelle.0911@gmail.com>
2019-09-12 15:03:29 -0700

    Finished signup component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Import packages
+import axios from 'axios'
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -7,10 +8,36 @@ import './App.css';
 import Content from './content/Content'
 import Header from './nav/Header'
 import Nav from './nav/Nav'
+import SERVER_URL from './constants'
 
 class App extends React.Component {
   state = {
     user: null
+  }
+
+  componentDidMount() {
+    // Go look for a token
+    this.getUser()
+  }
+
+  getUser = () => {
+    // See if there's a token
+    let token = localStorage.getItem('mernToken')
+
+    // If there's a token, try to use it to get the user info
+    if (token) {
+      console.log('token was', token)
+      axios.get(`${SERVER_URL}/auth/current/user`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      .then(response => {
+        console.log('Success', response)
+        this.setState({ user: response.data.user })
+      })
+      .catch(err => {
+        console.log('Error with token', err)
+      })
+    }
   }
 
   render() {
@@ -19,7 +46,7 @@ class App extends React.Component {
         <div className="App">
           <Nav user={this.state.user} />
           <Header />
-          <Content />
+          <Content updateUser={this.getUser} user={this.state.user} />
         </div>
       </Router>
     );
