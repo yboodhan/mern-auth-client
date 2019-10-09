8115679f57217beeca7e5db58b5913e49fb931da
brandiw <brandimichelle.0911@gmail.com>
2019-10-09 11:22:22 -0700

    Refactoring token so that it doesn't refresh unless expired

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import './App.css';
 import Content from './content/Content'
 import Header from './nav/Header'
 import Nav from './nav/Nav'
-// import SERVER_URL from './constants'
 
 class App extends React.Component {
   state = {
@@ -34,7 +33,7 @@ class App extends React.Component {
     if (token) {
       let decoded = jwtDecode(token)
 
-      // If the token is expired or it couldn't be decoded, refresh with the server
+      // If token is expired or not decodable, user is not logged in
       if (!decoded || Date.now() >= decoded.exp * 1000) {
         console.log('expired!')
         this.setState({ user: null })
@@ -47,30 +46,6 @@ class App extends React.Component {
       this.setState({ user: null })
     }
   }
-
-  // refreshToken = () => {
-  //   // See if there's a token
-  //   let token = localStorage.getItem('mernToken')
-
-  //   // If there's a token, try to use it to get the user info
-  //   if (token) {
-  //     fetch(`${SERVER_URL}/auth/current/user`, {
-  //       headers: { 'Authorization': `Bearer ${token}` }
-  //     })
-  //     .then(response => response.json())
-  //     .then(response => {
-  //       console.log('Success', response)
-  //       return response.token
-  //     })
-  //     .catch(err => {
-  //       console.log('Error with token', err)
-  //       return null
-  //     })
-  //   }
-  //   else {
-  //     return null
-  //   }
-  // }
 
   render() {
     return (
