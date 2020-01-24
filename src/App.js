78cac9cf1c834331f4dde2cece25e274592a8581
yboodhan <yboodhan@gmail.com>
2020-01-23 16:55:59 -0800

    Added decoder, addition auth for login

--- src/App.js
+++ src/App.js
@@ -16,25 +16,36 @@ const App = props => {
   // Define any onload actions (e.g., to look for the token)
   useEffect(() => {
     console.log('check for token')
+    decodeToken()
   }, [])
 
-  // Helper function to update the user
+  // Helper function to update the user (login, sign-up, and logout use this)
   const updateUser = (newToken) => {
     if (newToken) {
-      // Save the token and update the user with the token's info
-      
+      // Save the token
+      localStorage.setItem('userToken', newToken)
+      // Update the user with the token's info
+      decodeToken(newToken)
+    } else {
+      setUser(null)
     }
   }
 
   // Helper function to decode existing tokens
+  const decodeToken = (existingToken) => {
+    let token = existingToken || localStorage.getItem('userToken')
+
+    console.log('The token is:', token) // If null, no user logged in
+    // TODO: Decode token
+  }
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav user={user} updateUser={updateUser}/>
         <Header />
         <main>
-          <Content />
+          <Content user={user} updateUser={updateUser}/>
         </main>
         <Footer />
       </div>
--- src/content/pages/Login.js
+++ src/content/pages/Login.js
@@ -1,5 +1,6 @@
 // Packages
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
 
 const Login = props => {
   // Declare and initialize state variables
@@ -7,10 +8,48 @@ const Login = props => {
   let [message, setMessage] = useState('')
   let [password, setPassword] = useState('')
 
+  // Update the message whenever something else is typed
+  useEffect(() => {
+    setMessage('')
+  }, [email, password])
+
   // Event handlers
   const handleSubmit = e => {
     e.preventDefault()
-    // TODO: Fetch call to POST data
+    // Fetch call to POST data
+    fetch(`http://localhost:3000/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Success')
+      response.json()
+      .then( result => {
+        console.log('Response', response) // metadata, status etc.
+        console.log('Result', result) // stuff in the send
+        if (response.ok) {
+          // Update the user's token (back up in App.js)
+          props.updateUser( result.token )
+        } else {
+          setMessage(`${response.status} ${response.statusText}: ${result.message}`)
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      setMessage(`${err.toString()}`)
+    })
+  }
+
+  // If user exists, redirect to the profile page
+  if (props.user) {
+    return <Redirect to="/profile"/>
   }
 
   return (
